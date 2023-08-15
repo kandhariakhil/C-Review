1cff9ab6530762e93d7b6c24ff42af79023343db
kandhariakhil <axk751@case.edu>
2023-08-15 09:03:44 -0400

    Linked list review

--- Elements of Programming/Lecture 4- Linked Lists/Lecture4_Linked_Lists_Review.cpp
+++ Elements of Programming/Lecture 4- Linked Lists/Lecture4_Linked_Lists_Review.cpp
@@ -14,26 +14,119 @@ class LinkedList
 private:
     Node *headPtr;
 public:
+
     // Constructor
     LinkedList(){
         headPtr = nullptr;
     } 
+
     // Print items
-    void printLinkedList(){}
+    void printLinkedList(){
+        Node *p = new Node;
+        p = headPtr;
+        while (p != nullptr){
+            std::cout<<p->val<<std::endl;
+            p = p->nextNode;
+        }
+    }
     
-    // Add items 
-    void addItems(int v){
+    // Methods to add items:
+    void addToFront(int v){
         // At the top of the list:
-        // At the rear of the list:
-        // In between the list:
+        Node *p = new Node;
+        p->val = v;
+        p->nextNode = headPtr;
+        headPtr = p;
     }
 
+    void addToRear(int v){
+        // At the rear of the list:
+        if (headPtr==nullptr){
+            addToFront(v);
+        }
+        else{
+            Node *p = new Node;
+            p = headPtr;
+            while (p != nullptr){
+                p = p->nextNode;
+            }
+            Node *n = new Node;
+            n->val = v;
+            n->nextNode = nullptr;
+            p->nextNode = n;
+        }
+    }
+
+    void addItem(int v, int preInsertionVariable){
+        // In between the list:
+        
+        if (headPtr==nullptr){
+            addToFront(v);
+        }
+        // Here we use a search criteria of where the item belongs
+        // else if (criteria)
+        else{
+            Node *p = new Node;
+            while (p!=nullptr){
+                if (p->val == preInsertionVariable){
+                    break;
+                }
+                p=p->nextNode;
+            }
+            Node *n = new Node;
+            n->val = v;
+            n->nextNode = p->nextNode;
+            p->nextNode = n;
+        }
+    }
     // Delete items 
-    void delItems(int v){}
+    void delItems(int v){
+        if(headPtr == nullptr){
+            return;
+        }
+
+        if(headPtr->val == v){
+            Node *p = headPtr;
+            headPtr = p->nextNode;
+            delete p;
+            return;
+        }
+
+        else{
+            Node *p = headPtr;
+            while (p != nullptr){
+                if( p->nextNode != nullptr && p->nextNode->val == v){
+                    break;
+                }
+                p = p->nextNode;
+            }
+            if (p != nullptr){
+                Node *deleteNode = p->nextNode;
+                p->nextNode = deleteNode->nextNode;
+                delete deleteNode;
+            }
+        }
+    }
+
+    // Find item
+    bool findItem(int v){
+        Node *p = headPtr;
+        while (p != nullptr){
+            if(p->val == v){
+                return true;
+            }
+            p = p->nextNode;
+        }
+        return false;
+    }
 
     // Destructor
-    ~LinkedList(){}
-    
+    ~LinkedList(){
+        Node *p = headPtr;
+        while (p != nullptr){
+            Node *n = p->nextNode;
+            delete p;
+            p = n;
+        }
+    }    
 };
-
-
