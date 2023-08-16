d79fd77fec730f486cd2cd986f5d089748de34b9
kandhariakhil <axk751@case.edu>
2023-08-16 08:12:08 -0400

    Add to a doubly-linked list

--- Elements of Programming/Lecture 4- Linked Lists/Lecture4_Doubly_Linked_Lists_Assignment.cpp
+++ Elements of Programming/Lecture 4- Linked Lists/Lecture4_Doubly_Linked_Lists_Assignment.cpp
@@ -7,6 +7,7 @@ struct Node
 {
     int val;
     Node *nextNode;
+    Node *prevNode;
 };
 
 class DoublyLinkedList
@@ -22,13 +23,59 @@ public:
     // Print items
     void printLinkedList(){}
     
-    // Add items 
-    void addItems(int v){
-        // At the top of the list:
-        // At the rear of the list:
-        // In between the list:
+    // Add items
+    void addToFront(int v){
+        Node *p = new Node;
+        p->val = v;
+        p->nextNode = headPtr;
+        p->prevNode = nullptr;
+        if(headPtr != nullptr){
+            headPtr->prevNode = p;
+        }
+        headPtr = p;
     }
 
+    void addToRear(int v){
+        if (tailPtr == NULL){
+            addToFront(v);
+        }
+        else{
+            Node *p = new Node;
+            p->val = v;
+            tailPtr->nextNode = p;
+            p->nextNode = nullptr;
+            p->prevNode = tailPtr;
+            tailPtr = p;
+        }
+    }
+
+    void addInBetween(int v, int preInsertionVariable){
+        if (headPtr == nullptr){
+            addToFront(v);
+        }
+        else{
+            Node *p = headPtr;
+            // forward traversal
+            while (p != nullptr){
+                if (p->val == preInsertionVariable){
+                    break;
+                }
+                p=p->nextNode;
+            }
+            Node *n = new Node;
+            n->val = v;
+            n->prevNode = p;
+            n->nextNode = p->nextNode;
+            // If p is tail pointer then make checks and adjustments
+            if (p->nextNode == nullptr){
+                tailPtr = n;
+            }
+            else{
+                n->nextNode->prevNode = n;
+            }
+            p->nextNode = n;
+        }
+    }
     // Delete items 
     void delItems(int v){}
 
