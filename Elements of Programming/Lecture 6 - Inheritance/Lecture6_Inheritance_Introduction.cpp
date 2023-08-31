18fd5129a2460a7b7b8f60e3470e1de735fca7fa
kandhariakhil <axk751@case.edu>
2023-08-31 08:09:57 -0400

    private, protected and public members of base class

--- Elements of Programming/Lecture 6 - Inheritance/Lecture6_Inheritance_Introduction.cpp
+++ Elements of Programming/Lecture 6 - Inheritance/Lecture6_Inheritance_Introduction.cpp
@@ -5,7 +5,12 @@
 class Robot
 {
 private:
-    int m_x, m_y;
+    int m_x, m_y, m_battery;
+
+    //This method is hidden from the derived class
+    void setBaseBattery(int newBattery){
+        m_battery = newBattery;
+    }
 public:
     Robot(){
         m_x = 0;
@@ -27,6 +32,15 @@ public:
     int getY(){
         return m_y;
     }
+
+    virtual void moveRobot(){
+        printf("Robot will move 10 steps \n");
+    }
+
+protected:
+    void chargeBattery(){
+        printf("Battery is charging \n");
+    }
 };
 
 class shieldedRobot : public Robot
@@ -45,16 +59,58 @@ public:
     void setShield(int s){
         m_shield = s;
     }
+
+    void setPosition(int xPos, int yPos){
+        setX(xPos);
+        setY(yPos);
+    }
+
+    // The following lines will not run since the method setBaseBattery is private
+    // void setDerivedBattery(int battery){
+    //     setBaseBattery(battery); 
+    // }
+
+    void chargeRobot(){
+        chargeBattery();
+    }
+
+    virtual void moveRobot(){
+        printf("Shielded robot will move 5 steps \n");
+    }
+
+    // Calling the base class version of moveRobot instead of derived class version
+    void boostMoveRobot(){
+        printf("Robot has achieved boost and will move faster - ");
+        Robot::moveRobot();
+    }
+
 };
 
 int main(){
-    shieldedRobot robo1;
-    robo1.setX(5);
-    robo1.setShield(10);
-    int roboX = robo1.getX();
-    int roboS = robo1.getShield();
+    Robot robo;
+    shieldedRobot roboShielded;
+    roboShielded.setX(5);
+    roboShielded.setShield(10);
+    int roboX = roboShielded.getX();
+    int roboS = roboShielded.getShield();
 
     printf("Shield is %d and X position is %d \n", roboS, roboX);
 
+    roboShielded.setPosition(10,40);
+
+    roboX = roboShielded.getX();
+    int roboY = roboShielded.getY();
+
+    printf("X position is %d and Y position is %d \n", roboX, roboY);
+
+    roboShielded.chargeRobot();
+
+    // Virtual functions showcasing extension
+    robo.moveRobot();
+    roboShielded.moveRobot();
+    roboShielded.boostMoveRobot();
+
+    // The following line will still fail
+    // robo1.chargeBatter();
 }
 
