e49a94e9d766c3fd4952058ae592c775996065c8
kandhariakhil <axk751@case.edu>
2023-09-27 08:52:50 -0400

    Linked list review

---
+++ Elements of Programming/Midterm Review - 1/MR2_Linked_Lists.cpp
@@ -0,0 +1,155 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+struct Node
+{
+    int val;
+    Node *next;
+};
+
+class Linked_Lists
+{
+private:
+    Node *headPtr;
+public:
+    // Constructor - create an empty list by pointing headPtr to nullptr
+    Linked_Lists(){
+        headPtr = nullptr;
+    }
+
+    // Methods to add elements to the list
+
+    // Add element to front
+    void addToFront(int newVal){
+        // No elements in the list
+        Node *p = new Node;
+        p->val = newVal;
+        p->next = headPtr;
+        headPtr = p;
+    }
+
+    void addToRear(int newVal){
+        // No elements in the list
+        if (headPtr == nullptr){
+            addToFront(newVal);
+        }
+        // List has elements - traverse to the last node
+        else{
+            Node *p = new Node;
+            Node *temp = headPtr;
+            while (temp->next != nullptr){
+                temp = temp->next;
+            }
+            p->val = newVal;
+            p->next = nullptr;
+            temp->next = p;
+        }
+    }
+
+    void addElement(int newVal,int preVal){
+        bool insertFlag = true;
+        if (headPtr==nullptr){
+            addToFront(newVal);
+        }
+        else{
+            Node *temp = headPtr;
+            while(temp->val != preVal && temp->next != nullptr){
+                temp = temp->next;
+            }
+            if (temp->val == preVal && temp->next == nullptr){
+                    addToRear(newVal);
+                    insertFlag = false;
+                }  
+            if (temp->val != preVal && temp->next == nullptr){
+                cout<<"End of list reached, value "<<preVal<<" not found"<<endl;
+                insertFlag = false;
+            }
+            if (insertFlag == true) {
+                Node *p = new Node;
+                p->val = newVal;
+                p->next = temp->next;
+                temp->next = p;
+            }
+        }
+    }
+
+    void deleteItem(int delVal){
+        
+        if (headPtr ==nullptr){
+            cout<<"List is empty"<<endl;
+            return;
+        }
+
+        if (headPtr->val == delVal){
+            Node *temp = headPtr;
+            headPtr = temp->next;
+            delete temp;
+            return;
+        }
+        else{
+            Node *temp = headPtr;
+            while(temp != nullptr){
+                if(temp->next != nullptr && temp->next->val == delVal){
+                    break;
+                }
+                temp = temp->next;
+            }
+            if (temp != nullptr){
+                Node *delNode = temp->next;
+                temp->next = delNode->next;
+                delete delNode;
+            }
+            
+        }
+    }
+
+    void printList(){
+        Node *p = headPtr;
+        if (p == nullptr){
+            cout<<"List is empty"<<endl;
+        }
+        else{
+            while(p != nullptr){
+                cout<<p->val;
+                cout<<"->";
+                p = p->next;
+            }
+            cout<<"NULL"<<endl;
+        }
+    }
+    ~Linked_Lists(){
+        Node *temp = headPtr;
+        while(temp != nullptr){
+            Node *delNode = temp->next;
+            delete temp;
+            temp = delNode;
+        }
+    }
+};
+
+int main(){
+    Linked_Lists newList;
+    newList.printList();
+    newList.addToFront(5);
+    newList.addToFront(10);
+    newList.printList();
+    newList.addToRear(25);
+    newList.printList();
+    newList.addElement(36,10);
+    newList.printList();
+    newList.addElement(99,25);
+    newList.addElement(100,30);
+    newList.printList();
+    newList.addToRear(100);
+    newList.printList();
+    newList.deleteItem(100);
+    newList.printList();
+    newList.deleteItem(25);
+    newList.printList();
+}
+
+
