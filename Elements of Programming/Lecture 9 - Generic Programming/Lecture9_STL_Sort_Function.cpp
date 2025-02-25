2853b6254b7d73c37306e9d99822fe1a4890107b
kandhariakhil <axk751@case.edu>
2025-02-24 21:41:06 -0500

    Adding iterator in sort function

--- Elements of Programming/Lecture 9 - Generic Programming/Lecture9_STL_Sort_Function.cpp
+++ Elements of Programming/Lecture 9 - Generic Programming/Lecture9_STL_Sort_Function.cpp
@@ -10,9 +10,30 @@ using namespace std;
 int main(){
     vector<string> n;
 
-    n.push_back("Akhil");
+    vector<string>::iterator it;
+
     n.push_back("Jim");
     n.push_back("Alex");
+    n.push_back("Tim");
+    n.push_back("Akhil");
+    
+    it = n.begin();
+
+    while (it != n.end())
+    {
+        cout<<(*it)<<endl;
+        it++;
+    }
 
     sort(n.begin(),n.end()); //sort the whole vector
+
+    cout<<"Sorted list: "<<endl;
+
+    it = n.begin();
+
+    while (it != n.end())
+    {
+        cout<<(*it)<<endl;
+        it++;
+    }
 }
